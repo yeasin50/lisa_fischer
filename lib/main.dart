feac3d9f28ed4bddc334a6fe7ab9889e4b91e129
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-04-30 23:19:44 +0600

    main

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Md. Yeasin Sheikh',
+      title: 'Portfolios',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
