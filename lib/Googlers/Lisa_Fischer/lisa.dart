7565218a9b4b89c26dde4ed4e55d577cf2f57f59
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-04-30 16:38:30 +0600

    LisaFischer

---
+++ lib/Googlers/Lisa_Fischer/lisa.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LisaFischer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Text("Lisa"),
+    ));
+  }
+}
