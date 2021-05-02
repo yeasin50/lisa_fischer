b93d3a8966270813373ab3a786aeb3faca62143a
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-05-02 17:58:42 +0600

    cover2methods

--- lib/Googlers/Lisa_Fischer/components/header.dart
+++ lib/Googlers/Lisa_Fischer/components/header.dart
@@ -1,57 +1,39 @@
-
 import 'package:flutter/material.dart';
 import 'package:portfolio/Googlers/Lisa_Fischer/components/page_nav.dart';
 
-class LSHeader extends StatelessWidget {
-  const LSHeader({
-    Key? key,
-  }) : super(key: key);
-
-  
-  Padding buildLogo() {
+class LSHeader {
+  Image buildLogo({double width = 44}) {
     final String _personal_monogram =
         "assets/images/lisa_personal_monogram.png";
-    return Padding(
-      padding: EdgeInsets.all(10),
-      child: Image.asset(
-        _personal_monogram,
-        scale: 1,
-        fit: BoxFit.cover,
-        width: 43,
-      ),
+    return Image.asset(
+      _personal_monogram,
+      scale: 1,
+      fit: BoxFit.cover,
+      width: width,
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
+  Row navigators() {
     return Row(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        buildLogo(),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            PageNavigator(
-                title: "WORK",
-                isActive: true,
-                onClick: () {
-                  print("Page Navigate to Work");
-                }),
-            PageNavigator(
-                isActive: false,
-                title: "ABOUT",
-                onClick: () {
-                  print("Page Navigate to About");
-                }),
-            PageNavigator(
-                isActive: false,
-                title: "CONTACT",
-                onClick: () {
-                  print("Page Navigate to contact");
-                }),
-          ],
-        ),
+        PageNavigator(
+            title: "WORK",
+            isActive: true,
+            onClick: () {
+              print("Page Navigate to Work");
+            }),
+        PageNavigator(
+            isActive: false,
+            title: "ABOUT",
+            onClick: () {
+              print("Page Navigate to About");
+            }),
+        PageNavigator(
+            isActive: false,
+            title: "CONTACT",
+            onClick: () {
+              print("Page Navigate to contact");
+            }),
       ],
     );
   }
