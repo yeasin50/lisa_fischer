b1c355bf97d3e8065848b40518a7044e114c55a9
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-05-06 00:40:58 +0600

    changeMaxTabWidth

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 
 const double kMaxWidth = 1180.0;
 const double kMobileMaxWidth = 650.0;
-const double kTabletMaxWidth = 1100.0;
+const double kTabletMaxWidth = 1130.0;
 
 const Color kColorDash = const Color(0xff00b288);
 
