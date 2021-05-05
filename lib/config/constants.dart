5d151010cf12fedf469a393f4e9827750516c849
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-05-05 17:47:05 +0600

    const

---
+++ lib/config/constants.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+const double kMaxWidth = 1180.0;
+const double kMobileMaxWidth = 650.0;
+const double kTabletMaxWidth = 1100.0;
+
+const Color kColorDash = const Color(0xff00b288);
+
+const String kFproximaNova= "ProximaNova";
