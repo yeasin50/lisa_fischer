1a53730155a9596e9ba7b30475c7472e9951aa1c
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-05-06 00:57:58 +0600

    fileMove

---
+++ lib/Googlers/Lisa_Fischer/pages/Work/work_main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/Googlers/Lisa_Fischer/components/footer_text.dart';
+import 'package:portfolio/Googlers/Lisa_Fischer/components/social_icons.dart';
+import 'package:portfolio/config/constants.dart';
+import 'package:portfolio/config/max_width_container.dart';
+import 'package:portfolio/config/responsive.dart';
+import '../../components/header.dart';
+import '../About/body.dart';
+import 'body.dart';
+
+///`HomePage`
+class LisaFischerWorkMain extends StatelessWidget {
+  static const String routename = "LisaWork_Main";
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Responsive(
+          desktop: WorkDesktopTablet(),
+          tablet: WorkDesktopTablet(),
+          mobile: LFWorkBody(
+            griditemC: 1,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WorkDesktopTablet extends StatelessWidget {
+  const WorkDesktopTablet({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Align(
+          alignment: Alignment(0, 0),
+          child: MaxWidthContainer(
+            child: LFWorkBody(
+              griditemC: Responsive.isDesktop(context) ? 3 : 2,
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment(0, -1),
+          child: Container(
+            width: kTabletMaxWidth,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LSHeader().buildLogo(width: 44),
+                LSHeader().navigators(),
+              ],
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment(0, 1),
+          child: Container(
+            color: Colors.white,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FooterText(),
+                SocialIcons(),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
