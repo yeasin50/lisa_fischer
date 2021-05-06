aa132f2520794db8960a6d20826f76bf15def2f1
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-05-06 16:53:49 +0600

    separatedDevice

--- lib/Googlers/Lisa_Fischer/pages/About/lisa_about.dart
+++ lib/Googlers/Lisa_Fischer/pages/About/lisa_about.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/Googlers/Lisa_Fischer/components/footer_text.dart';
+import 'package:portfolio/Googlers/Lisa_Fischer/components/header.dart';
+import 'package:portfolio/Googlers/Lisa_Fischer/components/social_icons.dart';
+import 'package:portfolio/config/constants.dart';
 import 'package:portfolio/config/max_width_container.dart';
 import 'package:portfolio/config/responsive.dart';
 
@@ -11,20 +15,59 @@ class LisaAboutPage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Responsive(
-          mobile: Container(
-            color: Colors.green,
-            child: Text("Phone,"),
+          mobile: LSAboutPageBody(
+            gridItemCount: 1,
           ),
-          tablet: LSAboutPageBody(
-            gridItemCount: 3,
-          ),
-          desktop: MaxWidthContainer(
+          tablet: AboutDesktop(),
+          desktop: AboutDesktop(),
+        ),
+      ),
+    );
+  }
+}
+
+class AboutDesktop extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Align(
+          alignment: Alignment(0, 0),
+          child: MaxWidthContainer(
             child: LSAboutPageBody(
-              gridItemCount: 4,
+              gridItemCount: Responsive.isDesktop(context) ? 4 : 3,
             ),
           ),
         ),
-      ),
+        Align(
+          alignment: Alignment(0, -1),
+          child: Container(
+            width: kTabletMaxWidth,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LSHeader().buildLogo(width: 44),
+                LSHeader().navigators(),
+              ],
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment(0, 1),
+          child: Container(
+            color: Colors.white,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FooterText(),
+                SocialIcons(),
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
