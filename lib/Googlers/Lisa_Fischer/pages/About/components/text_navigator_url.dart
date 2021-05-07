654d0eba5ced58b0b4058b934220f1e46c2a04f4
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-05-07 23:59:56 +0600

    cleanUp

--- lib/Googlers/Lisa_Fischer/pages/About/components/text_navigator_url.dart
+++ lib/Googlers/Lisa_Fischer/pages/About/components/text_navigator_url.dart
@@ -17,19 +17,25 @@ class UrlNaVigator extends StatefulWidget {
 
 class _UrlNaVigatorState extends State<UrlNaVigator> {
   bool _isHover = false;
+
   @override
   Widget build(BuildContext context) {
     final TextStyle _textStyle = TextStyle(
+      /// there is attribute to handle space between underline and text
+      /// thats' why we are creating `Shadow as Main text` and setting original text Color to transparent
       shadows: [
         Shadow(
-          color: _isHover ? kColorDash : Colors.black,
-          offset: Offset(0, -5),
+          color: _isHover ? kColorDash : const Color(0xffa8a8a8),
+          offset: Offset(0, -4),
         )
       ],
+      fontFamily: kFproximaNova,
+      fontWeight: FontWeight.w700,
+      fontSize: 14.5,
       color: Colors.transparent,
       decoration: TextDecoration.underline,
       decorationColor: _isHover ? Colors.blue : Colors.transparent,
-      decorationThickness: 2,
+      decorationThickness: _isHover ? 2 : 0.0,
       decorationStyle: TextDecorationStyle.solid,
     );
     return InkWell(
@@ -47,7 +53,7 @@ class _UrlNaVigatorState extends State<UrlNaVigator> {
         padding: const EdgeInsets.only(top: 8),
         child: Text(
           widget.title,
-          textAlign: TextAlign.center,
+          textAlign: TextAlign.left,
           style: _textStyle,
         ),
       ),
