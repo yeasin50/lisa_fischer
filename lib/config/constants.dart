9deb360c1571c5bc50d5f2c7ca9eaea2a676396d
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-05-09 18:13:37 +0600

    addedTopAlignHeight

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
+
 const double kMaxWidth = 1180.0;
 const double kMobileMaxWidth = 650.0;
 const double kTabletMaxWidth = 1130.0;
@@ -9,3 +11,8 @@ const Color kColorDash = const Color(0xff00b288);
 const String kFproximaNova = "ProximaNova";
 
 const double kNormalTextHeight = 1.4;
+
+const double kTopLevelStackSpace = 150;
+
+/// The first child(`Main Body`) of Stack
+const Alignment kTopBodyStackChildAlignment = Alignment(0, -.85);
