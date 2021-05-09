b86befdec28ce0b97bcc91e5f4e69c80715a7ca7
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-05-10 00:40:00 +0600

    CustomRouteTransaction

---
+++ lib/Googlers/Lisa_Fischer/components/custom_route.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+/// this page route will start from `TopRight`
+Route pageRouteFromRT(
+  Function child, {
+  Offset startOffset = const Offset(0, -.8),
+}) {
+  print("type=> ${child.runtimeType} ");
+  return PageRouteBuilder(
+    transitionDuration: Duration(milliseconds: 500),
+    pageBuilder: (context, animation, secondaryAnimation) => child(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var curve = Curves.easeInOutQuad;
+
+      var tween = Tween(
+        /// begin [Offset()] is responsible for corner;
+        begin: startOffset,
+        end: Offset.zero,
+      ).chain(CurveTween(curve: curve));
+      var offsetAnimation = animation.drive(tween);
+      return SlideTransition(
+        position: offsetAnimation,
+        child: child,
+      );
+    },
+  );
+}
