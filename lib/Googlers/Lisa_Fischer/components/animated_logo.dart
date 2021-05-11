2bfa0dc0af7d385112ff1df29374844992dffe39
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-05-12 01:46:43 +0600

    addedScaleAnimation

--- lib/Googlers/Lisa_Fischer/components/animated_logo.dart
+++ lib/Googlers/Lisa_Fischer/components/animated_logo.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
+enum AnimationType {
+  rotate,
+  scaleX,
+}
+
 class AnimatedLogo extends StatefulWidget {
   final double logoWidth;
+  final AnimationType animType;
 
   const AnimatedLogo({
     Key? key,
-    this.logoWidth = 35,
+    this.logoWidth = 44,
+    this.animType = AnimationType.scaleX,
   }) : super(key: key);
   @override
   _AnimatedLogoState createState() => _AnimatedLogoState();
@@ -16,16 +23,37 @@ class _AnimatedLogoState extends State<AnimatedLogo>
   final String _personal_monogram = "assets/images/lisa_personal_monogram.png";
   late AnimationController _controller;
 
+  late Animation<double> _animX;
   late Animation<double> _animY;
   late Animation<double> _animZ;
+
+  _animationDuration() {
+    /// set `animation controller duration`
+    if (widget.animType == AnimationType.rotate)
+      return Duration(seconds: 5);
+    else
+      return Duration(seconds: 1);
+  }
 
   _initAnim() {
     _controller = new AnimationController(
       vsync: this,
-      duration: Duration(seconds: 5),
+      duration: _animationDuration(),
     )..addListener(() => setState(() {}))
         // ..repeat()
         ;
+
+    /// bounceInOut for [Get In Touch]=> [Contact page]
+    _animX = Tween<double>(begin: 3.5, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(
+          0.0,
+          1.0,
+          curve: Curves.bounceInOut,
+        ),
+      ),
+    );
 
     _animY = Tween<double>(begin: 0.0, end: 360.0).animate(
       CurvedAnimation(
@@ -48,7 +76,11 @@ class _AnimatedLogoState extends State<AnimatedLogo>
       ),
     );
 
-    _controller.forward();
+    /// little delay to load the page
+    if (widget.animType == AnimationType.rotate)
+      Future.delayed(Duration(seconds: 1), () => _controller.forward());
+    else
+      _controller.forward();
   }
 
   @override
@@ -67,18 +99,39 @@ class _AnimatedLogoState extends State<AnimatedLogo>
 
   @override
   Widget build(BuildContext context) {
+    if (widget.animType == AnimationType.rotate)
+      return _rotateAnimation();
+    else
+      return _transformScaleX();
+  }
+
+  Transform _transformScaleX() {
+    /// bounce/ scaleX for [Contact] page
+    return Transform(
+      transform: Matrix4.identity()..setEntry(0, 0, _animX.value),
+      alignment: FractionalOffset.centerLeft,
+      child: _buildImage(),
+    );
+  }
+
+  Transform _rotateAnimation() {
+    /// rotate animation for [Work] page
     return Transform(
       transform: Matrix4.identity()
         ..rotateY((0 - _animY.value) * _magicValue)
         ..rotateZ((0 - _animZ.value) * _magicValue),
       alignment: FractionalOffset.bottomLeft,
-      child: Image.asset(
-        _personal_monogram,
-        scale: 1,
-        fit: BoxFit.fitHeight,
-        width: widget.logoWidth,
-        height: widget.logoWidth * 1.5,
-      ),
+      child: _buildImage(),
+    );
+  }
+
+  Image _buildImage() {
+    return Image.asset(
+      _personal_monogram,
+      scale: 1,
+      fit: BoxFit.fitHeight,
+      width: widget.logoWidth,
+      height: widget.logoWidth * 1.5,
     );
   }
 }
