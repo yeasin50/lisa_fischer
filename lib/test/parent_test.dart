e2116d154332b3e3ab2b99c26705297fd6291eca
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-05-12 01:48:15 +0600

    waveAnimTest

--- lib/test/parent_test.dart
+++ lib/test/parent_test.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/Googlers/Lisa_Fischer/components/animated_logo.dart';
 import 'package:portfolio/Googlers/Lisa_Fischer/components/header.dart';
+import 'package:portfolio/Googlers/Lisa_Fischer/components/wave_animation.dart';
 
 class TestLab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(child: AnimatedLogo()),
+        body: Center(child: TestBody()),
       ),
     );
   }
@@ -20,18 +21,31 @@ class TestBody extends StatefulWidget {
 
 class _TestBodyState extends State<TestBody> with TickerProviderStateMixin {
   late AnimationController _controller;
-
+  late AnimationController _waveController;
+  late Animation<double> _animX;
   late Animation<double> _animY;
   late Animation<double> _animZ;
+  late Animation<double> _animWave;
 
   _initAnim() {
     _controller = new AnimationController(
       vsync: this,
-      duration: Duration(seconds: 5),
-    )..addListener(() => setState(() {}))
-        // ..repeat()
-        ;
+      duration: Duration(seconds: 1),
+    )
+      ..addListener(() => setState(() {}))
+      ..repeat();
 
+    /// bounceInOut for [Get In Touch]=> [Contact page]
+    _animX = Tween<double>(begin: 4.0, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(
+          0.0,
+          1.0,
+          curve: Curves.bounceInOut,
+        ),
+      ),
+    );
     _animY = Tween<double>(begin: 0.0, end: 360.0).animate(
       CurvedAnimation(
         parent: _controller,
@@ -53,19 +67,43 @@ class _TestBodyState extends State<TestBody> with TickerProviderStateMixin {
       ),
     );
 
-    _controller.forward();
+    // _controller.forward();
+  }
+
+  _initWave() {
+    _waveController = new AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    )..addListener(() => setState(() {}));
+
+    /// wave 👋 hand
+    _animWave = Tween<double>(begin: 0, end: 30).animate(
+      CurvedAnimation(
+        parent: _waveController,
+        curve: const Interval(
+          0.0,
+          1.0,
+          curve: Curves.easeInOut,
+        ),
+      ),
+    );
+
+    _waveController.repeat(reverse: true);
   }
 
   @override
   void dispose() {
     _controller.dispose();
+
+    _waveController.dispose();
     super.dispose();
   }
 
   @override
   void initState() {
     super.initState();
-    _initAnim();
+    // _initAnim();
+    _initWave();
   }
 
   double _magicValue = 0.0174533;
@@ -74,18 +112,26 @@ class _TestBodyState extends State<TestBody> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) => Container(
-        width: 100,
-        height: 100,
-        // color: Colors.blue.withOpacity(.1),
-        child: Transform(
-          transform: Matrix4.identity()
-            ..rotateY((0 - _animY.value) * _magicValue)
-            ..rotateZ((0 - _animZ.value) * _magicValue),
-          alignment: FractionalOffset.bottomLeft,
-          child: LSHeader().buildLogo(
-            width: 55,
-          ),
-        ),
+          width: 100,
+          height: 100,
+          // color: Colors.blue.withOpacity(.1),
+          child: WavingWidget(
+            child: Text(
+              "👋",
+              style: TextStyle(
+                fontSize: 40,
+              ),
+            ),
+          )),
+    );
+  }
+
+  Transform buildTransformLogo() {
+    return Transform(
+      transform: Matrix4.identity()..setEntry(0, 0, _animX.value),
+      alignment: FractionalOffset.centerLeft,
+      child: LSHeader().buildLogo(
+        width: 55,
       ),
     );
   }
