659f29b74d623d2e97108c9b2105d98688d1fb75
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-05-16 22:26:14 +0600

    itemDetails

---
+++ lib/Googlers/Lisa_Fischer/pages/GridItemDetails/BuyOnGoogle/components/rowItem.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/Googlers/Lisa_Fischer/components/text_styles.dart';
+import 'package:portfolio/Googlers/Lisa_Fischer/pages/About/components/text_helper.dart';
+import 'package:portfolio/config/constants.dart';
+
+/// GirdItems details helper=> Row
+class RowItem extends StatelessWidget {
+  final String title;
+  final Widget body;
+  final double width;
+
+  const RowItem({
+    Key? key,
+    required this.width,
+    required this.title,
+    required this.body,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      margin: EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 12,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontFamily: kFproximaNova,
+              fontSize: 12.5,
+              color: Colors.black,
+              fontWeight: FontWeight.w900,
+              letterSpacing: 1.5,
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Container(
+            margin: const EdgeInsets.only(
+              top: 6,
+              bottom: 15,
+            ),
+            width: 25,
+            height: 3,
+            color: kColorDash,
+          ),
+
+          ///main Text part
+          body,
+        ],
+      ),
+    );
+  }
+}
