f1bd1cabdff07992190e2b7846d22b4b069fe114
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-05-23 21:48:10 +0600

    BuyOnGoogleDetails

--- lib/Googlers/Lisa_Fischer/pages/GridItemDetails/BuyOnGoogle/buy_on_google.dart
+++ lib/Googlers/Lisa_Fischer/pages/GridItemDetails/BuyOnGoogle/buy_on_google.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/config/max_width_container.dart';
+import 'package:portfolio/config/responsive.dart';
+import 'package:portfolio/test/fadeAnimTest.dart';
 import './body.dart';
 
 class BuyOnGoogle extends StatelessWidget {
@@ -6,8 +9,28 @@ class BuyOnGoogle extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: DesktopBody(),
+        body: DesktopLayout(),
+
+        //  Responsive(
+        //   mobile: Container(),
+        //   tablet: Padding(
+        //     padding: const EdgeInsets.symmetric(
+        //       horizontal: 40,
+        //     ),
+        //     child: DesktopBody(),
+        //   ),
+        //   desktop: DesktopLayout(),
+        // ),
       ),
+    );
+  }
+}
+
+class DesktopLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaxWidthContainer(
+      child: DesktopBody(),
     );
   }
 }
