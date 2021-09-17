5387798fb85198f74effa69a56caf15dcc31320f
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-09-18 00:27:01 +0600

    emojiWrapper_fromAssets

---
+++ lib/widgets/widget.emojiWrapper.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+/// having trouble on emoji render, backUp plan
+class EmojiWrapper extends StatelessWidget {
+  const EmojiWrapper({
+    Key? key,
+    required this.imageName,
+    required this.size,
+  }) : super(key: key);
+
+  final String imageName;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      "emojis/$imageName.png",
+      fit: BoxFit.contain,
+      width: size,
+    );
+  }
+}
