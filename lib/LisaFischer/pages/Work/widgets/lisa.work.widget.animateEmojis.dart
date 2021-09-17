c2adf63f1df8abcfd1825c2230b480bc08ba3e37
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-09-18 00:26:00 +0600

    betterWayToAnimateHeaderEmojis

--- lib/LisaFischer/pages/Work/widgets/lisa.work.widget.animateEmojis.dart
+++ lib/LisaFischer/pages/Work/widgets/lisa.work.widget.animateEmojis.dart
@@ -1,6 +1,6 @@
-import 'package:easy_rich_text/easy_rich_text.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+import 'package:portfolio/widgets/widget.emojiWrapper.dart';
 
 class AnimateEmojis extends StatefulWidget {
   @override
@@ -9,84 +9,32 @@ class AnimateEmojis extends StatefulWidget {
 
 class _AnimateEmojisState extends State<AnimateEmojis>
     with SingleTickerProviderStateMixin {
-  final _emojis = ["👋", "👀", "👇", "💭"];
-  // final _emojis = ["A", "B", "C", "D"];
-
-  /// for counting Position
-  late AnimationController _controller;
-  late Animation _animation;
-
-  /// animating on List
-  final ItemScrollController itemScrollController = ItemScrollController();
-  final ItemPositionsListener itemPositionsListener =
-      ItemPositionsListener.create();
-
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
-      _initController();
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  _initController() {
-    /// 4 items = 4 sec;
-    _controller =
-        AnimationController(duration: const Duration(seconds: 4), vsync: this);
-
-    /// end on 3 because index 0to3=4
-    _animation = IntTween(begin: 0, end: 3).animate(_controller)
-      ..addListener(() {
-        // _scrollController.jumpTo(_animation.value);
-        //// `ScrollablePositionedList`
-        itemScrollController.scrollTo(
-          index: _animation.value,
-          duration: Duration(milliseconds: 200),
-          // curve:
-        );
-        setState(() {});
-      })
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed)
-          _controller.reverse();
-        else if (status == AnimationStatus.dismissed) _controller.forward();
-      });
-
-    _controller.forward();
-  }
+  // final _emojis = ["👋", "👀", "👇", "💭"];
+  final _emojiPaths = ['wave', 'look', 'down', 'thinking'];
 
   @override
   Widget build(BuildContext context) {
-    ////TODO:: adjust Font&BOx Size
-    return SizedBox(
-      width: 50,
-      height: 50,
-
-      /// if we use [ListView] you have calculate offset like
-      /// textSize* position
-      /// another Approch we can try [ScrollablePositionedList]
-      // child: ListView(
-      //   controller: _scrollController,
-      //   children: _items,
-      // ),
-      child: ScrollablePositionedList.builder(
-        physics: BouncingScrollPhysics(),
-        itemBuilder: (BuildContext context, int index) {
-          return EasyRichText(
-            _emojis[index],
-            defaultStyle: TextStyle(
-              fontSize: 33,
-            ),
-          );
-        },
-        itemCount: _emojis.length,
-        itemScrollController: itemScrollController,
-        itemPositionsListener: itemPositionsListener,
+    return Container(
+      width: 33 + (8 * 2),
+      alignment: Alignment.center,
+      child: CarouselSlider(
+        items: _emojiPaths
+            .map(
+              (name) => EmojiWrapper(
+                imageName: name,
+                size: 33,
+              ),
+            )
+            .toList(),
+        options: CarouselOptions(
+          scrollDirection: Axis.vertical,
+          scrollPhysics: BouncingScrollPhysics(),
+          autoPlay: true,
+          reverse: true,
+          height: 44 * 1.5,
+          aspectRatio: 9 / 9,
+          viewportFraction: 1,
+        ),
       ),
     );
   }
