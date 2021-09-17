653cec79441b8d3a2084c0f5667428fb10998516
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-09-17 22:35:34 +0600

    migrateToSelectable

--- lib/LisaFischer/widgets/lisa.widget.footerText.dart
+++ lib/LisaFischer/widgets/lisa.widget.footerText.dart
@@ -7,7 +7,7 @@ class FooterText extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Text(
+      child: SelectableText(
         "© LIsa Fischer 2021 | Designer | lisasuefischer@gmail.com",
         style: _footerTextStyle,
         textAlign: TextAlign.center,
