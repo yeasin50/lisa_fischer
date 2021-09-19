dfd753bf7a0b18496795bca1949f8a56d8a06e07
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-09-19 23:16:39 +0600

    unusedFiles

--- lib/LisaFischer/pages/About/utils/lisa.about.utils.var.dart
+++
@@ -1,5 +0,0 @@
-String get texts1 =>
-    "\nShe specializes in bridging the tenets of brand identity with UX/UI to create innovative and impactful design solutions for the modern age.";
-
-String get texts2 =>
-    "Aside from design, Lisa plays piano, runs long distances, and consistently binges episodes of 30 Rock.";
