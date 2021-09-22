952f51cb0e24ed16d69da6f7ffb9e197c855b543
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-09-22 22:01:45 +0600

    adddedMobileWrapper

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,3 +2,4 @@ export 'widget.circleNeonBorder.dart';
 export 'widget.rectNeonBorder.dart';
 export 'widget.desktopWrapper.dart';
 export 'widget.tabletWrapper.dart';
+export 'widget.mobileWrapper.dart';
