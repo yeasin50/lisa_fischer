90fe6047fd43f67563027cb1e4a87f35929f25ca
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-09-22 22:01:21 +0600

    easyApproachOFMenu

--- lib/LisaFischer/widgets/lisa.widget.mobileMenu.dart
+++ lib/LisaFischer/widgets/lisa.widget.mobileMenu.dart
@@ -1,111 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class LSMenu extends StatefulWidget {
-  @override
-  _LSMenuState createState() => _LSMenuState();
-}
-
-class _LSMenuState extends State<LSMenu> {
-  bool _isOpen = false;
-
+class LSMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-          width: double.infinity,
-          color: Colors.black,
-          padding: const EdgeInsets.only(bottom: 8, top: 4),
-          margin: const EdgeInsets.only(
-            bottom: 8,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: InkWell(
-                  onTap: () => setState(() => _isOpen = !_isOpen),
-                  onHover: (value) {
-                    if (value) setState(() => _isOpen = true);
-                  },
-                  child: Text("MENU",
-                      style: GoogleFonts.lato(
-                        color: Colors.white,
-                      )),
-                ),
+    return Container(
+      child: ExpansionTile(
+        trailing: SizedBox(),
+        backgroundColor: Colors.black,
+        collapsedBackgroundColor: Colors.black,
+        expandedAlignment: Alignment.center,
+        expandedCrossAxisAlignment: CrossAxisAlignment.center,
+        title: Center(
+          child: Transform.translate(
+            offset: Offset(16, 0),
+            child: Text(
+              "MENU",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.lato(
+                color: Colors.white,
               ),
-              if (_isOpen)
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-
-                  ///TODO:: set Navigator
-                  children: [
-                    MenuItemLS(
-                        text: "WORK",
-                        onPress: () {
-                          print("Work");
-                        }),
-                    MenuItemLS(
-                        text: "ABOUT",
-                        onPress: () {
-                          print("About Nav");
-                        }),
-                    MenuItemLS(
-                        text: "CONTACT",
-                        onPress: () {
-                          print("Contact Nav");
-                        }),
-                  ],
-                )
-            ],
-          )),
+            ),
+          ),
+        ),
+        children: [
+          MenuItemLS(
+              text: "WORK",
+              onPress: () {
+                print("Work");
+              }),
+          MenuItemLS(
+              text: "ABOUT",
+              onPress: () {
+                print("About Nav");
+              }),
+          MenuItemLS(
+              text: "CONTACT",
+              onPress: () {
+                print("Contact Nav");
+              }),
+        ],
+      ),
     );
   }
 }
 
-class MenuItemLS extends StatefulWidget {
+class MenuItemLS extends StatelessWidget {
   final String text;
   final Function onPress;
-
-  const MenuItemLS({required this.text, required this.onPress});
-
-  @override
-  _MenuItemLSState createState() => _MenuItemLSState();
-}
-
-class _MenuItemLSState extends State<MenuItemLS> {
-  bool _isHover = false;
+  const MenuItemLS({
+    Key? key,
+    required this.text,
+    required this.onPress,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => widget.onPress(),
-      onHover: (value) {
-        if (value)
-          setState(() => _isHover = true);
-        else
-          setState(() => _isHover = false);
-      },
-      child: Container(
-        padding: const EdgeInsets.symmetric(
-          vertical: 8,
-        ),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10, top: 4),
+      child: GestureDetector(
+        onTap: () => onPress(),
         child: Text(
-          widget.text,
+          text,
           style: GoogleFonts.lato(
             textStyle: TextStyle(
-              shadows: [
-                Shadow(
-                  color: Colors.white,
-                  offset: Offset(0, -5),
-                )
-              ],
-              color: Colors.transparent,
-              decoration: TextDecoration.underline,
-              decorationColor: _isHover ? Colors.blue : Colors.transparent,
-              decorationThickness: 2,
-              decorationStyle: TextDecorationStyle.solid,
+              color: Colors.white,
             ),
           ),
         ),
