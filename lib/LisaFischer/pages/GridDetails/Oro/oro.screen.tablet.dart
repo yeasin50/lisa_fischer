8e32afe16a854102163bdbd384a12ce083804aa6
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-09-27 21:38:29 +0600

    views

---
+++ lib/LisaFischer/pages/GridDetails/Oro/oro.screen.tablet.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/image_transaction.dart';
+import 'widgets/widgets.dart';
+
+class TabletView extends StatelessWidget {
+  const TabletView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) => Column(
+        children: [
+          // Intro(),
+          ImagesWithDetails(),
+        ],
+      ),
+    );
+  }
+}
