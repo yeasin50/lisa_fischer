57bb5f1052edc21f0674ae45c4cdf1658b673346
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-09-28 22:35:02 +0600

    thriveAwardsData

---
+++ lib/LisaFischer/pages/GridDetails/Thrive/utils/awards.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/widgets.dart';
+
+import '../../../../widgets/widgets.dart';
+
+final awardsOnThrive = [
+  HoverEffectOnLink(
+    key: UniqueKey(),
+    textInOrder: {
+      "Semifinalist in": false,
+      "Adobe Design Achievement Awards,": true,
+      "May 2016": false,
+    },
+    domainUrl: "http://www.adobeawards.com/us/live",
+  ),
+  HoverEffectOnLink(
+    key: UniqueKey(),
+    textInOrder: {
+      "Winner in": false,
+      "Applied Arts Magazine Awards,": true,
+      "July 2016": false,
+    },
+    domainUrl: "http://www.appliedartsmag.com/winners_gallery/",
+  ),
+];
