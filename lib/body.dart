64e1086c2d30da8f72bc86a8f07c332fb6cf2c26
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-09-29 23:20:54 +0600

    structLucere

--- lib/body.dart
+++ lib/body.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/LisaFischer/pages/GridDetails/Thrive/thrive.screen.dart';
 
 import 'LisaFischer/pages/GridDetails/BarcelonaMetroRedesign/BarcelonaMetroRedesign.dart';
 import 'LisaFischer/pages/GridDetails/GoogleShopping/googleShopping.screen.dart';
-import 'LisaFischer/pages/GridDetails/Oro/Oro.dart';
+
 import 'LisaFischer/pages/Work/lisa.work.screen.dart';
 
 class MyHomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Oro();
+  Widget build(BuildContext context) => Thrive();
 }
