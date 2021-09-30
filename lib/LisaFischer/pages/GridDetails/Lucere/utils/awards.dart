db1be1aa5862e5d7a13bfc49ac9294b173e057f7
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-09-30 20:20:39 +0600

    added_awardsDataOnLucere

---
+++ lib/LisaFischer/pages/GridDetails/Lucere/utils/awards.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/widgets.dart';
+
+import '../../../../widgets/widgets.dart';
+
+final awardsOnLucere = [
+  HoverEffectOnLink(
+    key: UniqueKey(),
+    textInOrder: {
+      "Semifinalist in": false,
+      "Adobe Design Achievement Awards,": true,
+      "May 2016": false,
+    },
+    domainUrl:
+        "http://www.graphis.com/entry/f30e4c10-f4eb-4a0f-9b9d-976973db5795/",
+  ),
+  HoverEffectOnLink(
+    key: UniqueKey(),
+    textInOrder: {
+      "Featured in": false,
+      "SCAD Portfolios,": true,
+      "Sept. 2015": false,
+    },
+    domainUrl: "http://portfolios.scad.edu/gallery/29336555/LUCERE",
+  ),
+];
