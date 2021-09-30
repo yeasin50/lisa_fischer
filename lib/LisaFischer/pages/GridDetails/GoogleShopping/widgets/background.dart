273526c6226ab19d60154ead547c0fcad2f1f9ee
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-09-30 21:22:45 +0600

    addedBlurHash

--- lib/LisaFischer/pages/GridDetails/GoogleShopping/widgets/background.dart
+++ lib/LisaFischer/pages/GridDetails/GoogleShopping/widgets/background.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blurhash/flutter_blurhash.dart';
 
 class Background extends StatelessWidget {
   const Background({Key? key}) : super(key: key);
@@ -7,12 +8,11 @@ class Background extends StatelessWidget {
       "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1519857189311-F5WHSJVM5LMJP4H529WB/logo+piecing.gif?format=2500w";
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Image.network(
-        _gifPath,
-        fit: BoxFit.cover,
-        loadingBuilder: (context, child, loadingProgress) => SizedBox(),
-      ),
+    return BlurHash(
+      hash:
+          '.4N-1Q~WbHxuoft7of-;ayWVWBj[j[j[E1axs;j[WBazfR%MWBj[offka|f7WBWUWBs:j[ayfP%MayoLayaya{ayR*j[ayjaofjtay%Mayj[j[ayayay',
+      image: _gifPath,
+      imageFit: BoxFit.cover,
     );
   }
 }
