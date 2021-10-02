e9485c1854cb292fc403c9806aa297cc03780abb
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-10-02 20:36:23 +0600

    simplifiedMaxWidh

--- lib/LisaFischer/pages/GridDetails/Leveled/leveled.screen.tablet.dart
+++ lib/LisaFischer/pages/GridDetails/Leveled/leveled.screen.tablet.dart
@@ -10,36 +10,39 @@ class TabletView extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: LayoutBuilder(
-        builder: (context, constraints) => Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ConstrainedBox(
-              constraints: BoxConstraints(
-                maxWidth: constraints.maxWidth * .85,
-              ),
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment(-1, -1),
-                    child: SizedBox(
-                      width: constraints.maxWidth * .4,
-                      child: P1LeveledIntro(),
+        builder: (context, constraints) {
+          final maxWidth = constraints.maxWidth * .85;
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxWidth: maxWidth,
+                ),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment(-1, -1),
+                      child: SizedBox(
+                        width: constraints.maxWidth * .4,
+                        child: P1LeveledIntro(),
+                      ),
                     ),
-                  ),
-                  P2LeveledRow(
-                    maxWidth: constraints.maxWidth * .85, //last one
-                  ),
-                  SizedBox(
-                    width: constraints.maxWidth * .7,
-                    child: P3ApproachLeveled(),
-                  ),
-                  P4Mood2(maxWidth: constraints.maxWidth * .85),
-                  P5Leveled(maxWidth: constraints.maxWidth * .85),
-                ],
+                    P2LeveledRow(
+                      maxWidth: maxWidth, //last one
+                    ),
+                    SizedBox(
+                      width: constraints.maxWidth * .7,
+                      child: P3ApproachLeveled(),
+                    ),
+                    P4Mood2(maxWidth: maxWidth),
+                    P5Leveled(maxWidth: maxWidth),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
+            ],
+          );
+        },
       ),
     );
   }
