280a65679c15ae6afb94e87764f385559f9ed450
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-10-02 22:10:55 +0600

    mainScreen

--- lib/LisaFischer/pages/GridDetails/Visda/visda.screen.dart
+++ lib/LisaFischer/pages/GridDetails/Visda/visda.screen.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/configs/configs.dart';
 
-import 'desktop.body.visda.dart';
+import '../../../../configs/configs.dart';
+import '../../../../widgets/widgets.dart';
+import 'Visda.dart';
 
-class VisdaGridItemScreen extends StatelessWidget {
-  const VisdaGridItemScreen({Key? key}) : super(key: key);
+class VisdaScreen extends StatelessWidget {
+  static final String routeName = "/work/visda";
+  const VisdaScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white,
         body: Responsive(
-          desktop: MaxWidthContainer(
-            child: VisdaDesktopBody(),
+          desktop: DesktopViewWrapper(
+            child: DesktopView(),
           ),
-          tablet: VisdaDesktopBody(),
-          mobile: VisdaDesktopBody(),
+          tablet: TabletViewWrapper(
+            child: TabletView(),
+          ),
+          mobile: MobieViewWrapper(
+            child: MobileView(),
+          ),
         ),
       ),
     );
