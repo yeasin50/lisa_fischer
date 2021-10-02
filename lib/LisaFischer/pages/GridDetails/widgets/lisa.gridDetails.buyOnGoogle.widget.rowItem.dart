fd7fc7eb2b3e490cca9dac956d53562f5d97ab5b
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-10-02 22:13:12 +0600

    addedFcommet

--- lib/LisaFischer/pages/GridDetails/widgets/lisa.gridDetails.buyOnGoogle.widget.rowItem.dart
+++ lib/LisaFischer/pages/GridDetails/widgets/lisa.gridDetails.buyOnGoogle.widget.rowItem.dart
@@ -5,7 +5,7 @@ import 'package:portfolio/configs/configs.dart';
 class RowItem extends StatelessWidget {
   final String title;
   final Widget body;
-  final double? width; //TODO: rm
+  final double? width; //TODO: rm, still finding a better way to handle this
 
   final bool isMobile;
 
@@ -17,32 +17,39 @@ class RowItem extends StatelessWidget {
     this.isMobile = false,
   }) : super(key: key);
 
+  get _body => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            title,
+            style: MyTextStyles().subtitle12.copyWith(color: Colors.black),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(
+              top: isMobile ? 4 : 8,
+              bottom: isMobile ? 6 : 12,
+            ),
+            child: Container(
+              width: 25,
+              height: 3,
+              color: kColorDash,
+            ),
+          ),
+
+          ///main Text part
+          body,
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Text(
-          title,
-          style: MyTextStyles().subtitle12.copyWith(color: Colors.black),
-        ),
-
-        Padding(
-          padding: EdgeInsets.only(
-            top: isMobile ? 4 : 8,
-            bottom: isMobile ? 6 : 12,
-          ),
-          child: Container(
-            width: 25,
-            height: 3,
-            color: kColorDash,
-          ),
-        ),
-
-        ///main Text part
-        body,
-      ],
-    );
+    return width == null
+        ? _body
+        : SizedBox(
+            width: width,
+            child: _body,
+          );
   }
 }
