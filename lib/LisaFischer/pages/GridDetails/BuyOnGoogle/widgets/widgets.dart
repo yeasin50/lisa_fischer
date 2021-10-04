37cfdfb3943f9ad4391698347470ca310b60924f
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-10-04 23:25:44 +0600

    addedSlideShowInBarrel

--- lib/LisaFischer/pages/GridDetails/BuyOnGoogle/widgets/widgets.dart
+++ lib/LisaFischer/pages/GridDetails/BuyOnGoogle/widgets/widgets.dart
@@ -2,4 +2,5 @@ export 'buyOnGoogle.widget.Challenge2solutions.dart';
 export 'buyOnGoogle.widget.dart';
 export 'buyOnGoogle.widget.gshop2actions.dart';
 export 'buyOnGoogle.widget.showCase.dart';
+export 'buyOnGoogle.widget.slideShowOnGShopAW.dart';
 export 'buyOnGoogle.widget.ytVideos.dart';
