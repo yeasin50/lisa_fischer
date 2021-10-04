8d5a98102a947c991b1f6effc2a2557ab9581a1c
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-10-04 23:25:12 +0600

    kindaImageTOGifMaking

---
+++ lib/LisaFischer/pages/GridDetails/BuyOnGoogle/utils/buyOnGoogle.utils.slideShowImageLinkOnActionWebsite.dart
@@ -0,0 +1,24 @@
+//* Links will be used to make kinda gif looks from images
+
+List<String> get imagesPathOfGifMaker => [
+      _firstImageLink,
+      _secondImageLink,
+      _thirdImgLink,
+      _fourthImageLink,
+      _fifthImageLink,
+    ];
+
+const String _firstImageLink =
+    "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1592248053264-8T1FZWE7YNIRS5NTD1MS/buy+on+google+_shoppping+actionss-higher+res-28.jpg?format=1500w";
+
+const String _secondImageLink =
+    "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1592248071324-HOO4YAY581WL98BP1ZYC/buy+on+google+_shoppping+actionss-higher+res-29.jpg?format=1500w";
+
+const String _thirdImgLink =
+    "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1592248061694-N5AKA85R58LLJ1VRUYIN/ke17ZwdGBToddI8pDm48kPtRZ_-Of0eqM9r1dRvliRl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0prfa1Z6IeUrCPboCAmmHZnFGLgB9zNT32qiWNNlcov3OavIiLKH5Iy-9eb-as2wug/buy+on+google+_shoppping+actionss-higher+res-31.jpg?format=2500w";
+
+const String _fourthImageLink =
+    "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1593327926056-CR91U2XX8QIYGRLQ70AO/buy+on+google+_shoppping+actionss-higher+res-332-03.png?format=1500w";
+
+const String _fifthImageLink =
+    "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1592249086745-V9WKJTADCX1VMLHLMHR3/buy+on+google+_shoppping+actionss-higher+res-33.jpg?format=1500w";
