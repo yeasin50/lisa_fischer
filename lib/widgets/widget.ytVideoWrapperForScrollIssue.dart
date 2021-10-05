a1a2b0e8f311108be19a87e0e30b491a4318623d
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-10-05 23:24:12 +0600

    addedWrapperToAvoidScrollIssue

---
+++ lib/widgets/widget.ytVideoWrapperForScrollIssue.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:pointer_interceptor/pointer_interceptor.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
+
+///* Cheating 🤐
+///* remove when we can fix Scroll-issue on yt package
+class YTVideoScrollIssueWrapper extends StatelessWidget {
+  const YTVideoScrollIssueWrapper({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  final YoutubePlayerController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        YoutubePlayerIFrame(
+          controller: controller,
+          aspectRatio: 16 / 9,
+        ),
+        PointerInterceptor(
+          child: AspectRatio(
+            aspectRatio: 16 / 9,
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            print(controller.initialVideoId);
+
+            launch(
+              "https://www.youtube.com/watch?v=${controller.initialVideoId}",
+            );
+          },
+          child: AspectRatio(
+            aspectRatio: 16 / 9,
+            child: Container(
+              color: Colors.transparent,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,5 +1,7 @@
 export 'widget.circleNeonBorder.dart';
-export 'widget.rectNeonBorder.dart';
 export 'widget.desktopWrapper.dart';
-export 'widget.tabletWrapper.dart';
+export 'widget.emojiWrapper.dart';
 export 'widget.mobileWrapper.dart';
+export 'widget.rectNeonBorder.dart';
+export 'widget.tabletWrapper.dart';
+export 'widget.ytVideoWrapperForScrollIssue.dart';
