8931148f13fce0c36a268c89f8a76f7e81ca9ad2
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-10-07 22:19:23 +0600

    improvedDesktopView

--- lib/LisaFischer/pages/About/lisa.about.desktop.dart
+++ lib/LisaFischer/pages/About/lisa.about.desktop.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-import '../../constants/lisa.const.divider.dart';
+import '../../../configs/config.constants.dart';
+import '../../constants/constants.dart';
 import '../../widgets/widgets.dart';
-import 'widgets/lisa.about.widget.briefInfo.dart';
 import 'widgets/widgets.dart';
 
 class DesktopView extends StatelessWidget {
@@ -10,89 +10,108 @@ class DesktopView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        //* Profile image and intro
-        Row(
-          children: [
-            ProfileImage(),
-            SizedBox(width: 20),
-            Expanded(
-              child: BriefInfo(),
-            ),
-          ],
-        ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final _maxWidth = constraints.maxWidth * .75;
 
-        spaceBetweenColumnItemsOnDesktop,
+        final _itemWidth = constraints.maxWidth * .75 / 4 - columnSpace.height!;
 
-        divider,
-
-        spaceBetweenColumnItemsOnDesktop,
-
-        ///*  details
-        GridView.count(
-          key: ValueKey("1st row of Qualification"),
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          crossAxisCount: 4,
-          childAspectRatio: 1.28,
-          crossAxisSpacing: 8,
-          mainAxisSpacing: 0,
-          children: [
-            LisaEducation(),
-            LisaExperience(),
-            LisaDesignSkills(),
-            LisaTechnicalSkills(),
-          ],
-        ),
-        GridView.count(
-          key: ValueKey("2nd row of Qualification"),
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          crossAxisCount: 4,
-          crossAxisSpacing: 8,
-          mainAxisSpacing: 0,
-          children: [
-            LisaAwards(),
-            LisaFeture(),
-            LisaPress(),
-            LisaWriting(),
-          ],
-        ),
-
-        spaceBetweenColumnItemsOnDesktop,
-
-        divider,
-
-        spaceBetweenColumnItemsOnDesktop,
-
-        GridView.count(
-          key: ValueKey("Contact Row"),
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          crossAxisCount: 4,
-          crossAxisSpacing: 0,
-          mainAxisSpacing: 8,
-          childAspectRatio: 4 / 2,
-          children: [
-            ConnectOnAbout(),
-            ContactOnAbout(),
-
-            //* Im invisible and controlling the Grid :)
-            SizedBox(),
-            Align(
-              alignment: Alignment.topRight,
-              child: CustomButton(
-                label: "GET IN TOUCH",
-                onTap: () {},
+        return SizedBox(
+          width: _maxWidth,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //* Profile image and intro
+              Row(
+                children: [
+                  SizedBox(
+                    width: constraints.maxWidth / 2,
+                    child: ProfileImage(),
+                  ),
+                  SizedBox(width: columnSpace.height!),
+                  Expanded(
+                    child: BriefInfo(),
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
-      ],
+
+              SizedBox(
+                height: columnSpace.height! * 3,
+              ),
+
+              divider,
+
+              SizedBox(
+                height: columnSpace.height! * 3,
+              ),
+
+              //*  details
+              SizedBox(
+                width: _maxWidth,
+                child: Wrap(
+                  alignment: WrapAlignment.spaceBetween,
+                  spacing: columnSpace.height!,
+                  runSpacing: columnSpace.height! * 2,
+                  children: [
+                    ...[
+                      ///FIXME: Edu text height change on scroll: hint=> just because of `runSpacing`
+                      LisaEducation(),
+                      LisaExperience(),
+                      LisaDesignSkills(),
+                      LisaTechnicalSkills(),
+                      LisaAwards(),
+                      LisaFeture(),
+                      LisaPress(),
+                      LisaWriting(),
+                    ].map(
+                      (item) => SizedBox(
+                        width: _itemWidth,
+                        child: item,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+
+              SizedBox(
+                height: columnSpace.height! * 1.5,
+              ),
+              divider,
+
+              SizedBox(
+                height: columnSpace.height! * 1.5,
+              ),
+
+              GridView.count(
+                key: ValueKey("Contact Row"),
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                crossAxisCount: 4,
+                crossAxisSpacing: 0,
+                mainAxisSpacing: 8,
+                childAspectRatio: 4 / 2,
+                children: [
+                  ConnectOnAbout(),
+                  ContactOnAbout(),
+
+                  //* Im invisible and controlling the Grid :)
+                  SizedBox(),
+
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: CustomButton(
+                      label: "GET IN TOUCH",
+                      onTap: () {
+                        ///TODO: route to contactPage
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
