3b52948fdb892f3c88dec925cb8c6e8fb9706368
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-10-10 20:19:55 +0600

    typoFix

--- lib/configs/config.colorPalette.dart
+++ lib/configs/config.colorPalette.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
 
+const Color kColorDash = const Color(0xff00b288);
 const linkTextColor = const Color(0xff363C3C);
 const linkTextColor2 = const Color(0xff141414);
