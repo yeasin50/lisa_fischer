ed25bbce3b84d812689244503443b712ed0386e8
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-10-14 20:44:01 +0600

    generalizedForNav2

--- lib/app.dart
+++ lib/app.dart
@@ -4,10 +4,21 @@ import 'package:google_fonts/google_fonts.dart';
 import 'constants/constants.dart';
 import 'routes/routes.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  AppRouterDelegate _routerDelegate = AppRouterDelegate();
+
+  AppRouteInformationParser _informationParser = AppRouteInformationParser();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routeInformationParser: _informationParser,
+      routerDelegate: _routerDelegate,
       title: 'Portfolios',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -15,8 +26,6 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: backgroundColor,
         primarySwatch: Colors.blue,
       ),
-      routes: routes,
-      initialRoute: '/',
     );
   }
 }
