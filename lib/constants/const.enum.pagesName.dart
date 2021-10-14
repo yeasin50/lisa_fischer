1b08657be04a09ddde320ccad8747d3141b1a682
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-10-14 20:46:19 +0600

    addedPagesNameAsEnum

---
+++ lib/constants/const.enum.pagesName.dart
@@ -0,0 +1,17 @@
+///*  n pages/tabs/screens name
+enum PageName {
+  work,
+  about,
+  contact,
+
+  //* work-pages
+  bercelonaMetroRedesign,
+  buyOnGoogle,
+  googleShopping,
+  leveled,
+  loppetWinterFestival,
+  lucere,
+  oro,
+  thrive,
+  visda,
+}
