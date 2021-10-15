f057bb1cef2d0e5a8fc2e50c2a365a8daa9ee7ed
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-10-15 22:05:52 +0600

    added workPages

--- lib/routes/route.routerDelegate.dart
+++ lib/routes/route.routerDelegate.dart
@@ -23,12 +23,47 @@ class AppRouterDelegate extends RouterDelegate<AppRoutePath>
       return AppRoutePath.work();
     }
     if (_pageName == PageName.contact) {
-      print("contactpage ccfig");
       return AppRoutePath.contact();
     }
     if (_pageName == PageName.about) {
-      print("aboutpage ccfig");
       return AppRoutePath.about();
+    }
+
+    //* workPages
+    if (_pageName == PageName.bercelonaMetroRedesign) {
+      return AppRoutePath.bercelonaMetroRedesign();
+    }
+
+    if (_pageName == PageName.buyOnGoogle) {
+      return AppRoutePath.buyOnGoogle();
+    }
+
+    if (_pageName == PageName.googleShopping) {
+      return AppRoutePath.googleShopping();
+    }
+
+    if (_pageName == PageName.leveled) {
+      return AppRoutePath.leveled();
+    }
+
+    if (_pageName == PageName.loppetWinterFestival) {
+      return AppRoutePath.loppetWinterFestival();
+    }
+
+    if (_pageName == PageName.lucere) {
+      return AppRoutePath.lucere();
+    }
+
+    if (_pageName == PageName.oro) {
+      return AppRoutePath.oro();
+    }
+
+    if (_pageName == PageName.thrive) {
+      return AppRoutePath.thrive();
+    }
+
+    if (_pageName == PageName.visda) {
+      return AppRoutePath.visda();
     }
 
     return AppRoutePath.unkwon();
@@ -58,6 +93,61 @@ class AppRouterDelegate extends RouterDelegate<AppRoutePath>
           MaterialPage(
             key: ValueKey<String>("ContactPage"),
             child: ContactPage(),
+          ),
+
+        ///TODO: Overlay with WorkPage
+        if (_pageName == PageName.buyOnGoogle)
+          MaterialPage(
+            key: ValueKey<String>("buyOnGooglePage"),
+            child: BuyOnGoolgePage(),
+          ),
+
+        if (_pageName == PageName.bercelonaMetroRedesign)
+          MaterialPage(
+            key: ValueKey<String>("bercelonaMetroRedesignPage"),
+            child: BercelonaMetroRedesignPage(),
+          ),
+
+        if (_pageName == PageName.googleShopping)
+          MaterialPage(
+            key: ValueKey<String>("googleShoppingPage"),
+            child: GoogleShpoingPage(),
+          ),
+
+        if (_pageName == PageName.leveled)
+          MaterialPage(
+            key: ValueKey<String>("leveledPage"),
+            child: LeveledPage(),
+          ),
+
+        if (_pageName == PageName.loppetWinterFestival)
+          MaterialPage(
+            key: ValueKey<String>("loppetWinterFestivalPage"),
+            child: LoppetWinterFestivalPage(),
+          ),
+
+        if (_pageName == PageName.lucere)
+          MaterialPage(
+            key: ValueKey<String>("lucerePage"),
+            child: LucerePage(),
+          ),
+
+        if (_pageName == PageName.oro)
+          MaterialPage(
+            key: ValueKey<String>("oroPage"),
+            child: OroPage(),
+          ),
+
+        if (_pageName == PageName.thrive)
+          MaterialPage(
+            key: ValueKey<String>("thrivePage"),
+            child: ThrivePage(),
+          ),
+
+        if (_pageName == PageName.visda)
+          MaterialPage(
+            key: ValueKey<String>("visdaPage"),
+            child: VisdaPage(),
           ),
       ],
       onPopPage: (route, result) {
@@ -92,5 +182,51 @@ class AppRouterDelegate extends RouterDelegate<AppRoutePath>
       show404 = false;
       _pageName = PageName.contact;
     }
+
+    ///* workPages
+    if (configuration.isBercelonaMetroRedesign) {
+      _setNewRoutePath(pName: PageName.bercelonaMetroRedesign);
+    }
+
+    if (configuration.isBuyOnGoogle) {
+      _setNewRoutePath(pName: PageName.buyOnGoogle);
+    }
+
+    if (configuration.isGoogleShopping) {
+      _setNewRoutePath(pName: PageName.googleShopping);
+    }
+
+    if (configuration.isLeveled) {
+      _setNewRoutePath(pName: PageName.leveled);
+    }
+
+    if (configuration.isLoppetWinterFestival) {
+      _setNewRoutePath(pName: PageName.loppetWinterFestival);
+    }
+
+    if (configuration.isLucere) {
+      _setNewRoutePath(pName: PageName.lucere);
+    }
+
+    if (configuration.isOro) {
+      _setNewRoutePath(pName: PageName.oro);
+    }
+
+    if (configuration.isThrive) {
+      _setNewRoutePath(pName: PageName.thrive);
+    }
+
+    if (configuration.isVisda) {
+      _setNewRoutePath(pName: PageName.visda);
+    }
+  }
+
+  ///* simplify the SingleLine
+  _setNewRoutePath({
+    required PageName? pName,
+    bool isErr = false,
+  }) {
+    show404 = isErr;
+    _pageName = pName;
   }
 }
