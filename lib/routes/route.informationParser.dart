99806d8bd2fb4b0be4a94c90a6cad96f9f08c712
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-10-16 21:53:46 +0600

    added workPages

--- lib/routes/route.informationParser.dart
+++ lib/routes/route.informationParser.dart
@@ -20,6 +20,50 @@ class AppRouteInformationParser extends RouteInformationParser<AppRoutePath> {
     if (uri.pathSegments[0] == PageName.contact.value)
       return AppRoutePath.contact();
 
+    //* Work pages: seems everything is after 
+    if (uri.pathSegments.length == 2) {
+      if (uri.pathSegments[0] == PageName.work.value) {
+        if (uri.pathSegments[1] == PageName.bercelonaMetroRedesign.value) {
+          return AppRoutePath.bercelonaMetroRedesign();
+        }
+
+        if (uri.pathSegments[1] == PageName.buyOnGoogle.value) {
+          return AppRoutePath.buyOnGoogle();
+        }
+
+        if (uri.pathSegments[1] == PageName.googleShopping.value) {
+          return AppRoutePath.googleShopping();
+        }
+
+        if (uri.pathSegments[1] == PageName.leveled.value) {
+          return AppRoutePath.leveled();
+        }
+
+        if (uri.pathSegments[1] == PageName.loppetWinterFestival.value) {
+          return AppRoutePath.loppetWinterFestival();
+        }
+
+        if (uri.pathSegments[1] == PageName.lucere.value) {
+          return AppRoutePath.lucere();
+        }
+
+        if (uri.pathSegments[1] == PageName.oro.value) {
+          return AppRoutePath.oro();
+        }
+
+        if (uri.pathSegments[1] == PageName.thrive.value) {
+          return AppRoutePath.thrive();
+        }
+
+        if (uri.pathSegments[1] == PageName.visda.value) {
+          return AppRoutePath.visda();
+        }
+      }
+
+      ///can be replaced with unkwon work page later
+      return AppRoutePath.unkwon();
+    }
+
     ///* `unkwon route`
     return AppRoutePath.unkwon();
   }
@@ -31,10 +75,44 @@ class AppRouteInformationParser extends RouteInformationParser<AppRoutePath> {
     }
 
     if (configuration.isAbout) {
-      return RouteInformation(location: PageName.about.location);
+      return PageName.about.location;
     }
-    if (configuration.isContact)
-      return RouteInformation(location: PageName.contact.location);
+    if (configuration.isContact) {
+      return PageName.contact.location;
+    }
+
+    //* work pagesInfo
+    if (configuration.isBercelonaMetroRedesign) {
+      return PageName.bercelonaMetroRedesign.location;
+    }
+
+    if (configuration.isBuyOnGoogle) {
+      return PageName.buyOnGoogle.location;
+    }
+
+    if (configuration.isGoogleShopping) {
+      return PageName.googleShopping.location;
+    }
+
+    if (configuration.isLeveled) {
+      return PageName.leveled.location;
+    }
+
+    if (configuration.isLucere) {
+      return PageName.lucere.location;
+    }
+
+    if (configuration.isOro) {
+      return PageName.oro.location;
+    }
+
+    if (configuration.isThrive) {
+      return PageName.thrive.location;
+    }
+
+    if (configuration.isVisda) {
+      return PageName.visda.location;
+    }
 
     return RouteInformation(location: '/');
   }
