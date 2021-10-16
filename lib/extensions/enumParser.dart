60f4d3b52313c91e6982ecf5888256e405f117a3
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-10-16 21:54:19 +0600

    simplified RouteInformation to get location

--- lib/extensions/enumParser.dart
+++ lib/extensions/enumParser.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
 import '../constants/constants.dart';
 
 extension PageExtension on PageName {
   String get value => this.toString().split('.').last;
-  String get location => "/${this.toString().split('.').last}";
+
+  RouteInformation get location => RouteInformation(
+        location:
+            "${this == PageName.about || this == PageName.contact ? '' : "work"}/${this.toString().split('.').last}",
+      );
 }
