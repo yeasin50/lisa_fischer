396f2a297c8309337c95e7ea315eaec015af738f
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-10-21 21:09:03 +0600

    imageHash_url separation

--- lib/screens/BuyOnGoogle/utils/buyOnGoogle.utils.imagesPath.dart
+++ lib/screens/BuyOnGoogle/utils/buyOnGoogle.utils.imagesPath.dart
@@ -1,12 +1,81 @@
-import 'package:flutter/material.dart';
+import '../../../model/model.dart';
 
-import '../../../configs/configs.dart';
-
-get gshopLogo => FadeInImage.assetNetwork(
-      placeholder: placeHolderImagePath,
-      image:
-          "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1590218888566-5HEMUQQQEU7I5D1YZQOB/ke17ZwdGBToddI8pDm48kHgeF6xw7HSVwCYTTeQdw017gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iE65AXCN5486i28K9GUUCgVjv5ZSo0OWMgFo2W4vcGZk1Rs35klMuCxeyNIaYEgSg/buy+on+google+cart+hero+in+elevation-08.png?format=1000w",
+HashImageData get gshopLogoImage => HashImageData(
+      imageUrl:
+          "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1593327372980-Q6HQFDRWZVOLL3GYCJKH/ke17ZwdGBToddI8pDm48kCRzb3JqnyRQ7VUq2pE7P097gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0ksOtWCjLd_P5vfQSnAKwnrZQ9grcTPXokHRV5Fh7vm2zjLo1yw71vv4_q2u1s0ysA/cart+on+blue+updated-33-33.png?format=750w",
+      hash:
+          "|3Q^2ax]G0\$,Q,l7?t-D0O?aayR:sps+S1WEspj]PDnjm%S|.8w14pKK~S%ioLs\$NtR;w}obNsadI9W;-=r?9GOY?awI9a-.W.f.wgjWOTaiwNkWJEjY\$cW?JEnh\$vS%I_.9jGV?XRkDnPfjXRaK~1fS0pfg~1a%5HoH=:",
     );
 
-get gshopImagePath =>
-    "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1590218888566-5HEMUQQQEU7I5D1YZQOB/ke17ZwdGBToddI8pDm48kHgeF6xw7HSVwCYTTeQdw017gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iE65AXCN5486i28K9GUUCgVjv5ZSo0OWMgFo2W4vcGZk1Rs35klMuCxeyNIaYEgSg/buy+on+google+cart+hero+in+elevation-08.png?format=1000w";
+HashImageData get gshopShowCase => HashImageData(
+      imageUrl:
+          "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1590468794072-PYSGV917AQK089V5TJ6D/ke17ZwdGBToddI8pDm48kEQmqQZdtGBB2XcSE0k8ACoUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYwL8IeDg6_3B-BRuF4nNrNcQkVuAT7tdErd0wQFEGFSnJW4iJZo6K6e5Za5EXsF7Xug5INyFhYLnFibc4K5-AtxqVck-AY33nSz_C37nTLCqg/zoomed%252Bin%252Bproduct%252Bcards-22.jpg?format=1000w",
+      hash:
+          "|KRfkAxuWFxvodR\$xaWB-;M|t7a#t7ayj@j[ogt7~qRQoeRia#ogRkocIU_3RjfORij[ogWBayIUM_ofWBofj@j?j[a#t79Ft7j]t7j@RiofRk%MxuofofoeWCa#ayt6j[-;WBj?WBWCogayofWBtRWBRjWBj?j[ayWBV@",
+    );
+
+//* on [P3Challenge2Solutions]
+
+HashImageData get solution1Cover => HashImageData(
+      imageUrl:
+          "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1590350017132-ZJWLDOX3GSITLN1K7GLT/ke17ZwdGBToddI8pDm48kBe3s72x4TW__9H-J8QPN6d7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0pQYXULvgm9t5Zb8hmAX2a-2QVMc0DOsEw99humzWVrFVBcZcfL7QrKE_xBa0lpg2Q/buy+on+google+images+for+portfolioo-12.png?format=750w",
+      hash:
+          "g7RftfTN5wD\$XU~TtS~qIUM{-;RPRPRjT2V@n}t7V?ayWA%LV?WBt7X9kDog4:-:%1IUt7M|od",
+    );
+
+HashImageData get solution2Cover => HashImageData(
+      imageUrl:
+          "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1593370185981-ZRTILUE0US7N72K1TZ70/ke17ZwdGBToddI8pDm48kPYL8vMf5MK395rQIEZQsvF7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0gkB-ogLx65Ep68Jc3FEurvoKLIolQEeIY4vOEJZJ4JJze7s2JxfUzCZ9mTlu3vZCA/roller+coaster-34-34-35-34-34.png?format=1500w",
+      hash:
+          "|DQmPE9h?IxXR.%NoaIUxusqo\$kBM_Rjt7RjoMxv~VIXWRM|%3t6IpxuRj9wM_xuxva{NGs.R*aJ?ZxCNL%1IUIo-pWFRQ-qE3M^t7xbxZW@oJM{oK%Ke-kDa#RjR%WAxvIVofs=oeRjWDxZV[ozxZWCf#RjofxuWFa#WA",
+    );
+
+HashImageData get solution3Cover => HashImageData(
+      imageUrl:
+          "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1593371429717-399E6BX3HCYTCEBIW9KW/ke17ZwdGBToddI8pDm48kGttJQ3fNJjEPJpWvJxnJ5x7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0qRGAtipPmBDa-K0OI3D5BM_VNprNPjtsnKQ3_2ravThonCkQUqLkSQrXw9s6QjMPw/buy+on+google+_shoppping+actionss-higher+res-332-03-32.png?format=750w",
+      hash:
+          "|3Qv|E%2yYcGxo-TMy\$,NE?vS\$aMv|R5S\$%zbcMdo\$nhs%T0IqnioJWT%f-qn\$kVS\$ogn~RijFt7tLS%M~v|%0Osb]s;IB-XnhX5OtyCr;DijY.7.mX9MJrqRoSjtQX4xu%Nnit6S\$Sio0nNnhS\$D\$jbxvWqo{obixnmIo",
+    );
+
+HashImageData get gshopOnWebSearch => HashImageData(
+      imageUrl:
+          "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1593358472652-A6IP5T382DZ1A7XSILMP/ke17ZwdGBToddI8pDm48kAHWeGNrnTNuaaRsQC5iQLWLPZHY9930KFuik00L7GzBKdq8kyNB6kzmmlKIayPDyODNVAVoT5JWrLXnFZMIhf1eKxxnxAGk445uFOpsZm_-/image-asset.gif?format=1000w",
+      hash:
+          "|3SY~#t7X;~p03IV0N%MEn%Mx[InMxaexux]RiM{00xuRPIV%Lxu%LRkt6kVRjoM-;%MD%D%t8%N00t6s,M{xtxtxtRjoIt8of.7RjIUbItRjYRi03WB%LocnhRkt7oes+_3ays:RjbHj[RjfiWBO^RjMxt7%fRPMxogt7",
+    );
+
+HashImageData get gshopOnMobileSearch => HashImageData(
+      imageUrl:
+          "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1593359373939-6PZMAHELDSJ4VAEKNIY9/ke17ZwdGBToddI8pDm48kAHWeGNrnTNuaaRsQC5iQLWLPZHY9930KFuik00L7GzBKdq8kyNB6kzmmlKIayPDyODNVAVoT5JWrLXnFZMIhf1eKxxnxAGk445uFOpsZm_-/image-asset.gif?format=750w",
+      hash:
+          "|9QJ.mt71noz^ct7D\$ofOa-;j[M{ayoffQt7j[M{0Lay~Vj[9Gayxvfkoc%Mj[Rjayj]fkofj[Rj9ba|?Fj[IVayt8fkocxufQfPfQj[fQayfQj[bxfQngfQWYfQkCfRoJogfQt7j[ayfQRjayt7.8fRD\$ayx]fRWBfQjY",
+    );
+
+//* [P4GShoppingActions]
+HashImageData get merchantWebsiteMarketing => HashImageData(
+      imageUrl:
+          "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1592206382479-5BCIRGPFXN1ATGFIUZDB/ke17ZwdGBToddI8pDm48kPtRZ_-Of0eqM9r1dRvliRl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0qoS64ZWQL6nhDAOCStJyMk7phXbdIvSPHOI4WOcEVpcoGVtCguu3ffHOCGxqAJ6Fg/yellow-16.png?format=750w",
+      hash:
+          "|4Sr_pRi=9?b=S%M,\$%Lw;E1M{xt%Mxuj[ofWBax+Dt7XCRjXVWBSkRkWt_3%MRjIUM{ayf6oft7}yxuNNM{IvWAW@ayt9?aofj[WBWBWBaya}WBrSRjtSt7kEofbJs:M|~WRjM{t7a#oLWBWAof-nWBRkj[ogofRkoeWC",
+    );
+
+HashImageData get emailMarketing => HashImageData(
+      imageUrl:
+          "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1617311844887-KILWYR9VHJXLAYAF3WW1/ke17ZwdGBToddI8pDm48kFmKdAJFcOk5fsAfzma6oat7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0lBmuC7q0oQgWRqy00Cl_4F3vpkLCkkli9g3-UXYeMmJ-1E2SEfIzOVqQPwVgHD5qw/buy+on+google+images+for+portfolio+email+marketing-15.png?format=750w",
+      hash:
+          "|7Qv:v%MpUad1W%MXnI@5O0KW@xut6_3ngRORPV?NoRNV@tS%OIqWY-pM|EdtQt8M|%MoLRPjXt70KxvxuR4-pt7RkNd%M.SIVRj%LemR*t7ofoh_MMxRQxvD*j]t6t6s:_LxsjtIV9Ht7ozof-p?vxvRjM{D\$s+xuM{%2",
+    );
+
+HashImageData get acheterSurGoogle => HashImageData(
+      imageUrl:
+          "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1590430636690-L93FH562JA8FZAYH00NF/ke17ZwdGBToddI8pDm48kNThfiiC1Ss5pt91TYgjpHJ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iy8Rj2bPXFyaluz0PeKicNeQIZkpfdXStHiv_LMkZT49G9mFXgpd49nBN3haodOMw/Acheter%2Bsur%2BGoogle_official%2Bbrand%2Blockup_for%2Bportfolio%2Bone%2Bpage-14.jpg?format=1000w",
+      hash:
+          "#SRpB]_3t7IUxut7ofM{t7~q9FWB%MRjWBWBxuRjD%%MfQRjofofj[Rjoft7ofWBayayj[fRaykB-;M{WBt7RjayayofWBD%xuofRjt7j[j[WBofofRjofofj@axjtofWB",
+    );
+
+HashImageData get ytMastHead => HashImageData(
+      imageUrl:
+          "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1590429082173-SZYTUEL9IQ76JETF6FOQ/ke17ZwdGBToddI8pDm48kA1pvmKWYbmcHStTUhjdsVd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0mIGv1bYyjw2Ebf4Z7encJBhjhWRA9vBfQElMMak6NDbLs51z2Nd-2qCPLgV8eBK_g/YT+masthead-18.png?format=1000w",
+      hash:
+          "#FRCrWxu^kf+-AMyv~V@r?~qWBIUWBRjogoet7ju~CRjIUjsR+xtWVozWV4moz-qoyt7V@WERPkB#8jabvoLR*bHf+axofIUj[xuj[ofaxWBaxWB~CR*IUayRks:X8ofW=",
+    );
