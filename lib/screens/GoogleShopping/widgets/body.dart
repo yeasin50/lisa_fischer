5809832d2d9d22432a9c1c4ae86ab86673bb368a
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-10-25 00:07:57 +0600

    improvedGoogleShoppingUI

--- lib/screens/GoogleShopping/widgets/body.dart
+++ lib/screens/GoogleShopping/widgets/body.dart
@@ -7,23 +7,18 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Background(),
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                LockStatus(),
-                SizedBox(height: 20),
-                AuthenticationOnGShop(),
-              ],
-            ),
-          ),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            LockStatus(),
+            SizedBox(height: 20),
+            AuthenticationOnGShop(),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
