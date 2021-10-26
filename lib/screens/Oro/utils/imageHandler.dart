a5aee28e3fd57556ed7dd03c340b6bff3e15c121
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-10-26 22:26:46 +0600

    future image loading case

---
+++ lib/screens/Oro/utils/imageHandler.dart
@@ -0,0 +1,20 @@
+const String _oroLogo_0 =
+    "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1490566448976-Q19JJOBOHAP9V9T0TVWP/ORO+updated_concept+visuals2-12.jpg";
+
+const String _briefInfo_1 =
+    "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1490566448976-Q19JJOBOHAP9V9T0TVWP/ORO+updated_concept+visuals2-12.jpg";
+
+const String _sateliteView_2 =
+    "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1490567298867-SNNAMVF3XV4HGGI1GMLC/image-asset.gif";
+
+const String _plane_3 =
+    "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1490566820582-GLI1YY78WN40BCFWBZXI/image-asset.jpeg";
+
+const String _airX_4 =
+    "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1490567071335-JB5JEWJZBVBUN1YT6B9F/oro_airplane+mockup_updated.jpg";
+
+const String _ticketsOverView_5 =
+    "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1490567128190-2ROXTD29ZOTTI1OOB5T5/ORO+BOARDING+PASSES+ISOMETRIC.jpg";
+
+const String _ticketsOverView_6 =
+    "https://images.squarespace-cdn.com/content/v1/547fe426e4b0dc192edb1ed5/1490567394013-JMPQ46RG4OWFTG0F48YK/image-asset.jpeg";
