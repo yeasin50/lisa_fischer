0c612b3d1c0dc739caa98cdab9f964cf6ad88d58
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-11-21 22:53:01 +0600

    logo tap navigate work/home page

--- lib/widgets/widget.animatedLogo.dart
+++ lib/widgets/widget.animatedLogo.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/provider.navigator.dart';
 
 enum AnimationType {
   rotate,
@@ -122,7 +125,13 @@ class _AnimatedLogoState extends State<AnimatedLogo>
         ..rotateY((0 - _animY.value) * _magicValue)
         ..rotateZ((0 - _animZ.value) * _magicValue),
       alignment: FractionalOffset.bottomLeft,
-      child: _buildImage(),
+      child: InkWell(
+        onTap: () {
+          final notifier = Provider.of<PageNotifier>(context, listen: false);
+          if (notifier.pageName != null) notifier.changeScreen(pageName: null);
+        },
+        child: _buildImage(),
+      ),
     );
   }
 
