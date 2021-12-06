bd014a69a593b836d9ac370cc94994111a36d52f
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-12-06 16:36:18 +0600

    404-views

---
+++ lib/screens/ErrorPage/desktop_view_error_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/widgets.dart';
+
+class DesktopView extends StatelessWidget {
+  const DesktopView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ErrorPageBodyBuilder();
+  }
+}
