044542df62f1b13c9f85c6da4ca90f582d26686a
yeasin50 <yeasinsheikh50@gmail.com>
2021-12-11 19:24:05 +0600

    fixed-mount

--- lib/screens/Work/lisa.work.screen.dart
+++ lib/screens/Work/lisa.work.screen.dart
@@ -1,37 +1,12 @@
-import 'dart:async';
 
 import 'package:flutter/material.dart';
 
 import '/configs/configs.dart';
-import '/utils/utils.dart';
-import '../../providers/providers.dart';
 import 'Work.dart';
 
 ///`HomePage`
-class WorkPage extends StatefulWidget {
+class WorkPage extends StatelessWidget {
   static const String routename = "/";
-
-  @override
-  State<WorkPage> createState() => _WorkPageState();
-}
-
-class _WorkPageState extends State<WorkPage> {
-  @override
-  void initState() {
-    super.initState();
-    Timer.run(
-      () async {
-        dynamicAppLabel(context: context, label: "Lisa Fischer");
-
-        final showDialog =
-            Provider.of<ProjectSetting>(context, listen: false).showDialog;
-        if (showDialog) {
-          await Future.delayed(ProjectSetting.initDialogDelay);
-          showProjectInfo(context);
-        }
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
